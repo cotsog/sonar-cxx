03ffb11a1ef2563e23af9bf68febb3b193dcb3d8
guwirth <guenter.wirth@etas.com>
2016-08-31 19:14:44 +0200

    add better logging for is_webui_up

--- integration-tests/features/environment.py
+++ integration-tests/features/environment.py
@@ -296,14 +296,21 @@ def wait_for_sonar(timeout, criteria):
     for _ in range(timeout):
         if criteria():
             return True
-        time.sleep(1)
+        time.sleep(5)
     return False
 
 
 def is_webui_up():
     try:
-        return urllib.urlopen(SONAR_URL).getcode() == 200
+        request = urllib.urlopen(SONAR_URL).getcode()
+        if request == 200
+            sys.stdout.write(GREEN + " %3i " % (request) + RESET)
+            return True
+        else
+            sys.stdout.write(RED + " %3i " % (request) + RESET)
+            return False
     except IOError:
+        sys.stdout.write(RED + " IOError " + RESET)
         return False
 
 
